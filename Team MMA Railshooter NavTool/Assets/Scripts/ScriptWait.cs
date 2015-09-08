da905ad553d9923a9e1b10be3e14a4c580fcfc6e
mdobson2 <mdobson2@stumail.jccc.edu>
2015-09-08 08:25:22 -0700

    Initialize
    Making the first run at Waypoints

---
+++ Team MMA Railshooter NavTool/Assets/Scripts/ScriptWait.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptWait : ScriptWaypoint {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        waypointTime -= Time.deltaTime;
+
+        if(waypointTime <= 0.0f)
+        {
+            //next movement
+        }
+	}
+}
---
+++ Team MMA Railshooter NavTool/Assets/Scripts/ScriptWaypoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptWaypoint  {
+
+    public enum movementTypes
+    {
+        WAIT,
+        MOVE,
+    };
+
+    public int moveType;
+    public float waypointTime;
+
+    public static  int numWaypoints;
+    public GameObject[] targetWaypoints = new GameObject[numWaypoints];
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach(GameObject wp in targetWaypoints)
+        {
+
+        }
+	}
+}
