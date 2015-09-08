336cc73c2b84aea64ed3dfc701b8b81977eb5d74
mdobson2 <svc-student@RC37812.student.directory.jccc>
2015-09-08 09:31:02 -0500

    Initialization 2

--- Team MMA Railshooter NavTool/Assets/Scripts/ScriptMove.cs
+++ Team MMA Railshooter NavTool/Assets/Scripts/ScriptMove.cs
@@ -4,14 +4,15 @@ using System.Collections;
 public class ScriptMove : ScriptWaypoint {
 
     public GameObject target;
+    Transform lastPoint;
 
 	// Use this for initialization
 	void Start () {
-	
+        lastPoint = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //transform.position = Vector3.Lerp(lastPoint.position, target.transform.position, waypointTime * Time.deltaTime);
 	}
 }
