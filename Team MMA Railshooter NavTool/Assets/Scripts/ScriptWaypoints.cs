17d51f190db382f808b60fdb57c6b0ffbdcae909
agentronin316 <svc-student@RC335G06.student.directory.jccc>
2015-09-12 09:55:01 -0500

    Started building data structure for waypoints
    Need to add in subclasses for superclass waypoint, but I have the
    variables and interaction baseline started.

---
+++ Team MMA Railshooter NavTool/Assets/Scripts/ScriptWaypoints.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptWaypoints : MonoBehaviour
+{
+    public WaypointType waypointType;
+
+    public MovementType movementType;
+    public float waitTime;
+    public float moveTime;
+    public ScriptWaypoints nextPoint;
+
+    public FacingType facingType;
+    public Transform[] targets;
+    public float[] facingTimes;
+    public float[] holdTimes;
+    
+    public EffectType effectType;
+    public float fadeInTime;
+    public float fadeOutTime;
+    public float effectDuration;
+
+    public virtual void GetWaypointInfo()
+    {
+        Debug.Log("Something is wrong here. Check overrides for GetWaypointInfo()");
+    }
+}
