c346f069051f7c338ae7eb4f6ccb8d24b315f922
agentronin316 <svc-student@RC335G06.student.directory.jccc>
2015-09-12 13:54:02 -0500

    Progress on UGC integration
    IO systems started.

---
+++ Team MMA Railshooter NavTool/Assets/Scripts/ScriptNavigation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// @Author Marshall Mason
+/// </summary>
+public class ScriptNavigation : MonoBehaviour {
+
+
+    public ScriptWaypoints[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
