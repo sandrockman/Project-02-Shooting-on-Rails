8ddb7aafd2f7b00fa14458d8bdfe8c6f5be0f8bf
mdobson2 <mdobson2@stumail.jccc.edu>
2015-09-12 21:10:46 -0700

    Bezier curve in
    Bezier curve has been added

--- Team MMA Railshooter NavTool/Assets/Editor/MovementEditorDrawer.cs
+++ Team MMA Railshooter NavTool/Assets/Editor/MovementEditorDrawer.cs
@@ -9,39 +9,59 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(ScriptMovements))]
 public class MovementEditorDrawer : PropertyDrawer
 {
+    bool movementShow = false;
     ScriptMovements waypointScript;
-    float extraHeight = 40f;
+    float extraHeight = 60f;
     float displaySize = 20f;
     float numDisplays = 0f;
     //int shouldSolidMove = 0;
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        EditorGUI.indentLevel++;
-        EditorGUI.BeginProperty(position, label, property);
-        SerializedProperty movementTypes = property.FindPropertyRelative("moveType");
-        numDisplays = 0;
+        
+            //EditorGUI.indentLevel++;
+            EditorGUI.BeginProperty(position, label, property);
+            SerializedProperty movementTypes = property.FindPropertyRelative("moveType");
+            numDisplays = 0;
 
-        //movmementType display block
-        //EditorGUILayout.PropertyField(movementTypes);
-        Rect movementTypesDisplay = new Rect(position.x, position.y + displaySize * numDisplays, position.width, 15f);
-        EditorGUI.PropertyField(movementTypesDisplay, movementTypes);
-        numDisplays += 1;
+            //movmementType display block
+            //EditorGUILayout.PropertyField(movementTypes);
+            Rect movementTypesDisplay = new Rect(position.x, position.y + displaySize * numDisplays, position.width, 15f);
+            EditorGUI.PropertyField(movementTypesDisplay, movementTypes);
+            numDisplays += 1;
 
-        //waypointTime display block
-        SerializedProperty waypointTime = property.FindPropertyRelative("waypointTime");
-        Rect waypointTimeDisplay = new Rect(position.x, position.y + displaySize, position.width, 15f);
-        EditorGUI.PropertyField(waypointTimeDisplay, waypointTime);
-        numDisplays += 1;
+            //waypointTime display block
+            SerializedProperty waypointTime = property.FindPropertyRelative("movementTime");
+            Rect waypointTimeDisplay = new Rect(position.x, position.y + displaySize, position.width, 15f);
+            EditorGUI.PropertyField(waypointTimeDisplay, waypointTime);
+            numDisplays += 1;
 
-        //target display block
-        SerializedProperty waypoint = property.FindPropertyRelative("waypoint");
-        Rect targetDisplay = new Rect(position.x, position.y + displaySize * numDisplays, position.width, 15f);
-        EditorGUI.PropertyField(targetDisplay, waypoint);
+            //target display block
+            if (movementTypes.enumValueIndex == (int)MovementTypes.MOVE)
+            {
+                SerializedProperty endWaypoint = property.FindPropertyRelative("endWaypoint");
+                Rect endWaypointDisplay = new Rect(position.x, position.y + displaySize * numDisplays, position.width, 15f);
+                EditorGUI.PropertyField(endWaypointDisplay, endWaypoint);
+            }
+
+            if (movementTypes.enumValueIndex == (int)MovementTypes.BEZIER)
+            {
+
+                SerializedProperty endWaypoint = property.FindPropertyRelative("endWaypoint");
+                Rect endWaypointDisplay = new Rect(position.x, position.y + displaySize * numDisplays, position.width, 15f);
+                EditorGUI.PropertyField(endWaypointDisplay, endWaypoint);
+                numDisplays += 1;
+
+                SerializedProperty curveWaypoint = property.FindPropertyRelative("curveWaypoint");
+                Rect curveWaypointDisplay = new Rect(position.x, position.y + displaySize * numDisplays, position.width, 15f);
+                EditorGUI.PropertyField(curveWaypointDisplay, curveWaypoint);
+            }
 
 
-        EditorGUI.EndProperty();
-        EditorGUI.indentLevel--;
+
+            EditorGUI.EndProperty();
+        
+        //EditorGUI.indentLevel--;
     }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
--- Team MMA Railshooter NavTool/Assets/Scripts/ScriptMovements.cs
+++ Team MMA Railshooter NavTool/Assets/Scripts/ScriptMovements.cs
@@ -10,14 +10,17 @@ public class ScriptMovements
 {
 
     [Tooltip("The amount of time that the player will take to complete this waypoint")]
-    public float waypointTime;
+    public float movementTime;
 
     [Tooltip("The type of movement that this waypoint will use")]
     public MovementTypes moveType;
 
     [Tooltip("The target for this movement")]
-    public GameObject waypoint;
+    public GameObject endWaypoint;
 
-    public Vector3 target;
+    [Tooltip("The curve for the bezier curve")]
+    public GameObject curveWaypoint;
 
+    [Tooltip("Determine if the window is folded out")]
+    public bool showInEditor = true;
 }
